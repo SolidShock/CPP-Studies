846625884d4223197e7222d9baa2093dc8249000
SolidShock <lopexsw@gmail.com>
2021-10-26 11:55:54 +0300

    02 Headers, forward declarations.
    Headers, forward declarations.

---
+++ Cpp studies/Cpp studies/add.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/*To maximize the chance that missing includes will be flagged by compiler, order your #includes as follows :
+
+The paired header file
+Other headers from your project
+3rd party library headers
+Standard library headers
+The headers for each grouping should be sorted alphabetically.
+*/
+
+int add(int x, int y);
